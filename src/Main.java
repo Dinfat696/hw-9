c271b0915b617b915a9996c53aeb08631d416e9c
dinarfathrahmanov <dinfat@mail.ru>
2023-02-24 11:44:51 +0300

    сделана задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        /*task2();
-        task3();
+        task2();
+        /*task3();
         task4();*/
 
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 
     public static void task1() {
         System.out.println("Задача 1");
+        int[ ] arr = generateRandomArray();
+        int sum=0;
+        for(int index=0; index<arr.length;index++){
+            sum=sum+arr[index];
+            System.out.println("Сумма трат за месяц составила " + sum+ " рублей");
+        }
     }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[ ] arr = generateRandomArray();
 }
